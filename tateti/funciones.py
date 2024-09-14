98e0df81db331347678c7e2c416b23c3f97a4ae6
agus grandon <agustina107.grandon@gmail.com>
2024-09-13 21:22:52 -0300

    cree el tateti

---
+++ tateti/funciones.py
@@ -0,0 +1,17 @@
+'''
+1) Crear una función que permita leer cada línea del archivo csv y convertir cada lectura en una matriz de 3X3.
+2) Crear una función que reciba la matriz anterior y la muestre.
+3) Crear una función que analice el contenido de la matriz y determine si gano la X, la O o si hubo empate.
+# (Tienen que hacer varias funciones para analizar las líneas verticales, horizontales o diagonales).
+# Repetir el punto 2 y 3 por cada línea del archivo.
+4) Antes de finalizar el programa mostrar las estadísticas:
+# Cuántas veces ganó la X
+# Cuántas veces ganó la O
+# Cuántas veces hubo empate
+
+ARCHIVO CSV
+XXOOOXXOO,
+OOXXXOOXO,
+OXXOOOXXO,
+XOXXXOXOO
+'''
